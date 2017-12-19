0b00b5346e03c6624621fa708177549e8db47b6e
criss50 <clopezc@institutobenitojuarez.edu.ec>
2017-12-18 20:14:21 -0500

    nuevo usuario

--- REGISTROS/USUARIO.js
+++ REGISTROS/USUARIO.js
@@ -1,18 +1,14 @@
 var urlWS = "";
 $(document).ready(function(){
-   urlWS = "http://172.16.0.9/segundo/SubastasSorteos/server/";
+   urlWS = "http://subasort01.byethost24.com/server/";
    leer(0);
 });
 
 
-function limpiar(){
-    document.getElementById("apellidoUsuario").value = "";
-    document.getElementById("nombreUsuario").value = "";
-}
 
 function leerfiltrado(){
-    filtro = document.getElementById("NombreBuscar").value;
-    urltorequest = urlWS +"nombreUsuario/leer_filtrado";
+    filtro = document.getElementById("nombreUsuarioBuscar").value;
+    urltorequest = urlWS +"Usuario/leer_filtrado";
     $.ajax({
         type: "get",
         url: urltorequest+"?columna=nombre&tipo_filtro=contiene&filtro="+filtro,
@@ -34,7 +30,7 @@ function leer(id){
     if(id==0){
         urltorequest = urlWS +"nombreUsuario/leer";
     }else{
-        urltorequest = urlWS +"nombreUsuario/leer?id="+apellidoUsuario;
+        urltorequest = urlWS +"Usuario/leer?id="+id;
     }
     $.ajax({
         type: "get",
@@ -42,11 +38,11 @@ function leer(id){
         async:true,
         success:  function (respuesta) {
            toshow = JSON.parse(respuesta);
-           cabeceraTabla = "<table class=\"table table-condensed\"><thead><tr><th>id</th><th>Nombre</th></tr></thead><tbody>";
+           cabeceraTabla = "<table class=\"table table-condensed\"><thead><tr><th>nombreUsuario</th><th>apellidoUsuario</th><th>direccionUsuario</th><th>telfUsuario</th><th>correoUsuario</th></tr></thead><tbody>";
            pieTabla = "</tbody></table>";
            contenidoTabla = "";
            $(toshow).each(function(key,value){
-                contenidoTabla=contenidoTabla+"<tr><td>"+value.id+"</td><td>"+value.nombreUsuario+"</td></tr>";
+                contenidoTabla=contenidoTabla+"<tr><td>"+value.nombreUsuario+"</td><td>"+value.apellidoUsuario+"</td></tr>"+value.direccionUsuario+"</td></tr>"+value.telfUsuario+"</td></tr>"+value.correoUsuario+"</td></tr>";
            });
            document.getElementById("respuesta").innerHTML=cabeceraTabla+contenidoTabla+pieTabla;
         }
@@ -55,17 +51,21 @@ function leer(id){
 }
 
 function borrar(){
-    id = document.getElementById("id").value;
-    urltorequest = urlWS +"nombreUsuario/borrar?id="+id;
+    nombreUsuario = document.getElementById("nombreUsuario").value ;
+    apellidaUsuario = document.getElementById("apellidoUsuario").value ;
+    direccionUsuario = document.getElementById("direccionUsuario").value ;
+    telfUsuario = document.getElementById("telfUsuario").value ;
+    correoUsuario = document.getElementById("correoUsuario").value ;
+    urltorequest = urlWS +"Usuario/borrar?id="+id;
     $.ajax({
         type: "get",
         url: urltorequest,
         async:false,
         success:  function (respuesta) {
             if(respuesta=="false"){
-                alert("Error al borrar el registro " + id + ".");
+                alert("Error al borrar el registro " + nombreUsuario + "."+ apellidoUsuario + "."+ direccionUsuario + "."+ telfUsuario + "."+ correoUsuario + ".");
             }else{
-                alert("Registro borrado: " + id + ".");
+                alert("Registro borrado: " + nombreUsuario + "."+ apellidoUsuario + "."+ direccionUsuario + "."+ telfUsuario + "."+ correoUsuario + ".");
             }
         }
     });
@@ -73,13 +73,16 @@ function borrar(){
 }
 
 function crear(){
-    id = document.getElementById("id").value;
-    descripcion = document.getElementById("descripcion").value;
-    urltorequest = urlWS +"nombreUsuario/crear";
+    nombreUsuario = document.getElementById("nombreUsuario").value ;
+    apellidaUsuario = document.getElementById("apellidoUsuario").value ;
+    direccionUsuario = document.getElementById("direccionUsuario").value ;
+    telfUsuario = document.getElementById("telfUsuario").value ;
+    correoUsuario = document.getElementById("correoUsuario").value ;
+    urltorequest = urlWS +"Usuario/crear";
     $.ajax({
         type: "post",
         url: urltorequest,
-        data:JSON.stringify({id: id, descripcion: descripcion}),
+        data:JSON.stringify({nombreUsuario: nombreUsuario, apellidoUsuario: apellidoUsuario,direccionUsuario:direccionUsuario,telfUsuario:telfUsuario,correoUsuario:correoUsuario}),
         async:false,
         success:  function (respuesta) {
             if(respuesta=="false"){
@@ -89,17 +92,20 @@ function crear(){
             }
         }
     });
+
     leer(0);
-}
 
 function actualizar(){
-    id = document.getElementById("id").value;
-    descripcion = document.getElementById("descripcion").value;
-    urltorequest = urlWS +"nombreUsuario/actualizar";
+    nombreUsuario = document.getElementById("nombreUsuario").value;
+    apellidoUsuario = document.getElementById("apellidoUsuario").value;
+    direccionUsuario = document.getElementById("direccionUsuario").value ;
+    telfUsuario = document.getElementById("telfUsuario").value ;
+    correoUsuario = document.getElementById("correoUsuario").value;
+    urltorequest = urlWS +"Usuario/actualizar";
     $.ajax({
         type: "post",
         url: urltorequest,
-        data:JSON.stringify({id: id, descripcion: descripcion}),
+        data:JSON.stringify({nombreUsuario: nombreUsuario, apellidoUsuario: apellidoUsuario,direccionUsuario:direccionUsuario,telfUsuario:telfUsuario,correoUsuario:correoUsuario}),
         async:false,
         success:  function (respuesta) {
             if(respuesta=="false"){
@@ -110,4 +116,5 @@ function actualizar(){
         }
     });
     leer(0);
+}
 }
