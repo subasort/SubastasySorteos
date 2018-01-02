c8cb0d892965a65bebef71675812b23079c45de6
criss50 <clopezc@institutobenitojuarez.edu.ec>
2018-01-02 17:53:20 -0500

    prueba funcional crear

--- REGISTROS/pruebades/prueba2.js
+++ REGISTROS/pruebades/prueba2.js
@@ -1,25 +1,34 @@
 var urlWS = "";
-$(document).ready(function() {
- 
+$(document).ready(function () {
     urlWS = "http://subasort01.byethost24.com/server/";
+
 });
 
-function crear(){
+function crear() {
     idUsuario = document.getElementById("idUsuario").value;
+    ciUsuario = document.getElementById("ciUsuario").value;
     nombreUsuario = document.getElementById("nombreUsuario").value;
-    urlWS +"Usuario/crear";
+    apellidoUsuario = document.getElementById("apellidoUsuario").value;
+    direccionUsuario = document.getElementById("direccionUsuario").value;
+    telfUsuario = document.getElementById("telfUsuario").value;
+    correoUsuario = document.getElementById("correoUsuario").value;
+    urltorequest = urlWS + "Usuario/crear";
     $.ajax({
         type: "post",
         url: urltorequest,
-        data:JSON.stringify({idUsuario: idUsuario, nombreUsuario: nombreUsuario}),
-        async:true,
+        data: JSON.stringify({
+            idUsuario: idUsuario, ciUsuario: ciUsuario, nombreUsuario: nombreUsuario,
+            apellidoUsuario: apellidoUsuario, direccionUsuario: direccionUsuario,
+            telfUsuario: telfUsuario, correoUsuario: correoUsuario
+        }),
+        async: true,
         success: function (respuesta) {
-            if(respuesta=="false"){
+            if (respuesta == "false") {
                 alert("Registro creado");
-            }else{
+            } else {
                 alert("registro no se pudo crear")
             }
         }
     });
-    leer();
 }
+
